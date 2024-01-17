e34d4e9a19a261c1b383ba53c0035bcb00bc9d1e
EthanBarlevy <98473724+EthanBarlevy@users.noreply.github.com>
2024-01-17 10:00:35 -0700

    added in variables and showed some things

--- NIM-TeamYes!/MainWindow.xaml.cs
+++ NIM-TeamYes!/MainWindow.xaml.cs
@@ -16,9 +16,31 @@ namespace NIM_TeamYes_
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		private int[] Matches = { 1, 3, 5, 7 };
+		private int SelectedRow = -1;
+		private int MatchesSelected = -1;
+		private bool Turn = true;
+		private bool IsGameOver = false;
+
 		public MainWindow()
 		{
 			InitializeComponent();
 		}
+
+		public void OnMatchClicked(object sender, RoutedEventArgs e)
+		{
+			if (true) // check selected row class of the button
+			{ 
+				MatchesSelected++;
+			}
+            else
+            {
+				MatchesSelected = 1;
+				// set selected row
+				// call resetrow
+            }
+
+
+        }
 	}
 }
